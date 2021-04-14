acfac0aee45922633b4928ee13c3de39f740b3b2
nickbartek <55357835+nickbartek@users.noreply.github.com>
2021-04-14 07:27:34 +0200

    former button songPlayer don't know what function does

--- src/App.js
+++ src/App.js
@@ -5,37 +5,29 @@ function Heading({ title }) {
   return <h1>{title}</h1>;
 }
 
-function SongPlayer({
-  showControls = false,
-  song,
-  songfor,
-  ...remainingAudioProps
-}) {
+function SongPlayer({ showControls = false, song, onPreviousSongClick }) {
   const audioRef = useRef();
-  const audioRefform = useRef();
   var [showControls, setshow] = useState(false);
-
-  const { audioUrlform, coverUrlform } = songfor;
   const { audioUrl, coverUrl } = song;
-  console.log(audioRef, audioRefform);
+
+  console.log(audioUrl);
+  console.log(audioRef);
+
+  function playformer() {
+    audioRef.current.play();
+    onPreviousSongClick();
+  }
+
   return (
     <section className="SongPlayer">
       <Heading title="Music Player" />
       <img width="250" height="250" src={coverUrl} alt="Song cover" />
-      <audio
-        ref={audioRef}
-        key={audioUrl}
-        controls={showControls}
-        {...remainingAudioProps}
-      >
+      <audio ref={audioRef} key={audioUrl} controls={showControls}>
         <source src={audioUrl} />
       </audio>
 
-      <audio ref={audioRefform} key={audioUrlform}>
-        <source src={audioUrlform} />
-      </audio>
       <div>
-        <button onClick={() => audioRefform.current.play()}>former</button>
+        <button onClick={() => playformer()}>former</button>
         <button onClick={() => audioRef.current.play()}>Play</button>
         <button onClick={() => audioRef.current.pause()}>Pause</button>
         <button onClick={() => setshow(!showControls)}>hide/show</button>
@@ -72,8 +64,8 @@ export default function App() {
   }, []);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [formerSongInd, setFormerInd] = useState(0);
-  const currentSong = songs[currentSongIndex];
-  const formerSong = songs[formerSongInd];
+  var currentSong = songs[currentSongIndex];
+  //const formerSong = songs[formerSongInd];
 
   function handleSelectSong(selectedSong) {
     const audioIndex = songs.findIndex(
@@ -82,16 +74,25 @@ export default function App() {
     if (audioIndex >= 0) {
       setCurrentSongIndex(audioIndex); //
       setFormerInd(currentSongIndex); //setFormerIndex
-      console.log(formerSongInd, currentSongIndex);
+      console.log("former, current", formerSongInd, currentSongIndex);
     }
   }
+  function handlePreviousSongClick() {
+    currentSong = songs[formerSongInd];
+    console.log("handlePreviousSongClick", currentSong);
+  }
+
   return (
     <div className="App">
       {songs.length === 0 ? (
         "Loading..."
       ) : (
         <>
-          <SongPlayer song={currentSong} songfor={formerSong} loop />
+          <SongPlayer
+            song={currentSong}
+            onPreviousSongClick={handlePreviousSongClick}
+            loop
+          />
           <Songs>
             <Heading title="Songs" />
             <ul>
